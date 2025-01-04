f19a077d18c1d8d51121b057a94f06e2ba7f585a
Greg Oliveira <gregoryomelo@gmail.com>
2025-01-04 10:28:32 -0300

    feat(exercicio_02): resolver segunda tarefa

---
+++ exercicio_02.py
@@ -0,0 +1,10 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+# Coletar primeiro valor
+primeiro_valor = int( input("Digite o primeiro valor: ") )
+
+# Coletar segundo valor
+segundo_valor = int( input("Digite o segundo valor: ") )
+
+# Exibir o resultado
+print( primeiro_valor + segundo_valor)

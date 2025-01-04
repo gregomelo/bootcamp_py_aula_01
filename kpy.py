648dde1f4667a6298d584a660bcfa86abcfb55f4
Greg Oliveira <gregoryomelo@gmail.com>
2025-01-04 13:43:07 -0300

    style(kpi.py): code quality review

--- kpy.py
+++ kpy.py
@@ -17,24 +17,24 @@
 #
 # 4. O cálculo do KPI do bônus de 2024 é de 1000 + salario * bônus
 #
-# 5. Finalmente, o programa deve imprimir uma mensagem no seguinte 
+# 5. Finalmente, o programa deve imprimir uma mensagem no seguinte
 # formato: "Olá [nome], o seu valor bônus foi de 5000".
 #
 # 6. Salve esse script python como kpi.py
 #
 # 7. Faça uma documentação simples de como executar esse programa,
 # utilize o README.
-# 
+#
 # 8. Salve no Git e no Github.
 
 # Coletando o nome do usuario
 nome_do_usuario = input("Digite o nome do usuário: ")
 
 # Coletando o valor do salário
-valor_salario = float( input("Digite o valor do salário: ") )
+valor_salario = float(input("Digite o valor do salário: "))
 
 # Calculando o bônus do usuário
 valor_bonus = 1000 + valor_salario * 1.5
 
 # Retornando o resultado
-print( f"Olá {nome_do_usuario}, o seu bônus foi de {valor_bonus}." )
+print(f"Olá {nome_do_usuario}, o seu bônus foi de {valor_bonus}.")
